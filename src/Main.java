68ab08ad38bee507151b7d14edbd359c413eef80
Bryan Capps <bryanacapps@gmail.com>
2017-03-28 10:59:41 -0500

    Making table view more MVC like

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,22 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Objects;
-
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-import models.Song;
 
 public class Main extends Application {
-	private final static Path musicFolder = Paths.get(System.getProperty("user.home"), "Music", "iTunes",
-			"iTunes Media", "Music");
-	private TableView<Song> table = new TableView<>();
-	private ObservableList<Song> items;
-	private MediaPlayer mediaPlayer;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-		primaryStage.setTitle("Hello World");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Boomer Tuner");
 
-		final VBox vbox = new VBox();
-		vbox.setSpacing(5);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-		Scene scene = new Scene(vbox, 800, 575);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-		TableColumn<Song, Integer> trackCol = new TableColumn<>("Track");
-		trackCol.setPrefWidth(50);
-		trackCol.setCellValueFactory(new PropertyValueFactory<Song, Integer>("track"));
-
-		TableColumn<Song, String> titleCol = new TableColumn<>("Title");
-		titleCol.setPrefWidth(200);
-		titleCol.setCellValueFactory(new PropertyValueFactory<Song, String>("title"));
-
-		TableColumn<Song, String> artistCol = new TableColumn<>("Artist");
-		artistCol.setPrefWidth(200);
-		artistCol.setCellValueFactory(new PropertyValueFactory<Song, String>("artist"));
-
-		TableColumn<Song, String> albumCol = new TableColumn<>("Album");
-		albumCol.setPrefWidth(200);
-		albumCol.setCellValueFactory(new PropertyValueFactory<Song, String>("album"));
-
-		table.getColumns().addAll(Arrays.asList(trackCol, titleCol, artistCol, albumCol));
-		vbox.getChildren().addAll(table);
-
-		loadSongs(table);
-	}
-
-	private void loadSongs(TableView<Song> list) {
-		items = FXCollections.observableArrayList();
-
-		table.setItems(items);
-
-		try {
-			Files.walk(musicFolder, 5)
-					.filter(path -> path.toString().toLowerCase().endsWith(".mp3")
-							|| path.toString().toLowerCase().endsWith(".m4a"))
-					.map(path -> Song.from(path.toUri())).filter(Objects::nonNull).forEachOrdered(items::add);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		list.getSelectionModel().selectedItemProperty().addListener((ov, oldValue, newValue) -> {
-			if (mediaPlayer != null)
-				mediaPlayer.stop();
-			Media media = new Media(newValue.getUri().toString());
-			mediaPlayer = new MediaPlayer(media);
-			mediaPlayer.play();
-		});
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/models/Song.java
+++ src/models/Song.java
@@ -1,9 +1,5 @@
 package models;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
@@ -12,6 +8,11 @@ import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.images.Artwork;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 
 /**
  * Created by bryancapps on 3/26/17.
@@ -22,12 +23,14 @@ public class Song {
     public final String album;
     public final int track;
     public final URI uri;
+    public final Artwork artwork;
 
-    public Song(String title, String artist, String album, int track, URI uri) {
+    public Song(String title, String artist, String album, int track, Artwork artwork, URI uri) {
         this.title = title;
         this.artist = artist;
         this.album = album;
         this.track = track;
+        this.artwork = artwork;
         this.uri = uri;
     }
 
@@ -39,33 +42,14 @@ public class Song {
             String album = tag.getFirst(FieldKey.ALBUM);
             String title = tag.getFirst(FieldKey.TITLE);
             String trackStr = tag.getFirst(FieldKey.TRACK);
+            Artwork artwork = tag.getFirstArtwork();
             
             int track = trackStr.isEmpty() ? -1 : Integer.valueOf(trackStr);
-            return new Song(title, artist, album, track, uri);
-        } catch (CannotReadException | IOException | TagException | InvalidAudioFrameException | ReadOnlyFileException | NumberFormatException e) {
+            return new Song(title, artist, album, track, artwork, uri);
+        } catch (CannotReadException | IOException | TagException | InvalidAudioFrameException | ReadOnlyFileException e) {
             e.printStackTrace();
             return null;
         }
-    }
-    
-    public Integer getTrack() {
-    	return track;
-    }
-    
-    public String getTitle() {
-    	return title;
-    }
-    
-    public String getArtist() {
-    	return artist;
-    }
-    
-    public String getAlbum() {
-    	return album;
-    }
-
-    public URI getUri() {
-        return uri;
     }
 
     @Override
@@ -79,6 +63,7 @@ public class Song {
         if (!title.equals(song.title)) return false;
         if (!artist.equals(song.artist)) return false;
         if (!album.equals(song.album)) return false;
+        if (!artwork.equals(song.artwork)) return false;
         return uri.equals(song.uri);
     }
 
@@ -89,6 +74,7 @@ public class Song {
         result = 31 * result + album.hashCode();
         result = 31 * result + track;
         result = 31 * result + uri.hashCode();
+        result = 31 * result + artwork.hashCode();
         return result;
     }
 
