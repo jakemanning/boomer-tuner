f3d55d2e5b12fc5cb603f776648b953322bb9d14
Aaron Wilson <aaronnw11@gmail.com>
2017-04-24 14:08:13 -0500

    Removed some extra remaining code.

--- src/artists/ArtistsView.java
+++ src/artists/ArtistsView.java
@@ -35,11 +35,7 @@ public class ArtistsView extends SplitPane implements CategoryView, DirectoryLis
 	}
 
 	private void initializeDetailView() {
-		if (model.isDirectorySelected() && MediaLibrary.instance().getAlbums().size() > 0) {
-			detail.setPlaceholder(new Label("Select an artist from the list"));
-		} else {
-			detail.setPlaceholder(new Label("Import media to view artists"));
-		}
+		detail.setPlaceholder(new Label("Import media to view artists"));
 		detail.getColumns().remove(2); // remove artist column
 		model.selectedArtistProperty().addListener((observable, oldValue, newValue) -> {
 			ObservableList<Song> items = MediaLibrary.instance().getSongs()
